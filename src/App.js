0fc2731ca208d0275df8cbf753e2c0161537ca1c
Tatyana19012 <chichika@ukr.net>
2023-05-20 17:03:30 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is coded by Tetiana Chychyka and is{" "}
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://github.com/Tatyana19012/react-weather-app"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is coded by Tetiana Chychyka and is{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/Tatyana19012/react-weather-app"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
