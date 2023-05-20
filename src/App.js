876083d4b9c595808e83eaa2ecfc9710a770b21b
Tatyana19012 <chichika@ukr.net>
2023-05-20 16:21:20 +0200

    1

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project is coded by Tetiana Chychyka and is{" "}
-        <a
-          href="https://github.com/Tatyana19012/react-weather-app"
-          target="_blank"
-        >
+        <a href="https://github.com/Tatyana19012/react-weather-app">
           open-sourced on GitHub
         </a>
       </footer>
