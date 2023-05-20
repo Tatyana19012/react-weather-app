9e29f271803c60e4cb2d5accf623c6a77ebf4233
Tatyana19012 <chichika@ukr.net>
2023-05-20 15:38:52 +0200

    Added GitHub

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is coded by Tetiana Chychyka and is{" "}
+        <a
+          href="https://github.com/Tatyana19012/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
